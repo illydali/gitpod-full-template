2c27729178b6d37bc2b55454d6bc186887d84940
illydali <ili.zaynal@gmail.com>
2022-04-17 15:27:06 +0000

    fixed image view

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -3,19 +3,23 @@ const router = express.Router();
 
 const cloudinary = require('cloudinary')
 
+// setup cloudinary
 cloudinary.config({
     'api_key': process.env.CLOUDINARY_API_KEY,
     'api_secret': process.env.CLOUDINARY_SECRET
 })
 
 router.get('/sign', async (req,res)=>{
-    // retrieve the parameters we need to send to cloudinary
+    // retrieve the params to sign
+    // Why use JSON.parse?
+    // because req.query.params_to_string is a JSON formatted string
+    // so we use JSON.parse to convert from JSON string to JSON object
     const params_to_sign = JSON.parse(req.query.params_to_sign);
 
     // retrieve our cloudinary api secret from the environment
     const apiSecret = process.env.CLOUDINARY_API_SECRET
 
-    // get the signature (aka CSRF)
+    // get the signature from cloudinary
     const signature = cloudinary.utils.api_sign_request(params_to_sign, apiSecret);
     
     res.send(signature);
--- routes/products.js
+++ routes/products.js
@@ -71,9 +71,9 @@ router.get('/create', checkIfAuthenticated, async (req, res) => {
     const productForm = createProductForm(allCategories, allTags);
     res.render('products/create', {
         'form': productForm.toHTML(bootstrapField),
-        cloudinaryName: process.env.CLOUDINARY_NAME,
-        cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-        cloudinaryPreset: process.env.CLOUDINARY_UPLOAD_PRESET
+        'cloudinaryName': process.env.CLOUDINARY_NAME,
+        'cloudinaryApiKey': process.env.CLOUDINARY_API_KEY,
+        'cloudinaryPreset': process.env.CLOUDINARY_UPLOAD_PRESET
     })
 })
 
@@ -99,6 +99,7 @@ router.post('/create', checkIfAuthenticated, async (req, res) => {
             product.set('cost', form.data.cost);
             product.set('description', form.data.description);
             product.set('category_id', form.data.category_id);
+            product.set('image_url', form.data.image_url);
             await product.save();
 
             // we can create the M:N relationship after the product is created
@@ -115,7 +116,10 @@ router.post('/create', checkIfAuthenticated, async (req, res) => {
         },
         'error': async (form) => {
             res.render('products/create', {
-                'form': form.toHTML(bootstrapField)
+                'form': form.toHTML(bootstrapField),
+                'cloudinaryName': process.env.CLOUDINARY_NAME,
+                'cloudinaryApiKey':process.env.CLOUDINARY_API_KEY,
+                'cloudinaryPreset':process.env.CLOUDINARY_UPLOAD_PRESET
             })
         }
     })
@@ -157,9 +161,9 @@ router.get('/:id/update', async (req, res) => {
         'form': form.toHTML(bootstrapField),
         'product': product.toJSON(),
         // 2 - send to the HBS file the cloudinary information
-        cloudinaryName: process.env.CLOUDINARY_NAME,
-        cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-        cloudinaryPreset: process.env.CLOUDINARY_UPLOAD_PRESET
+        'cloudinaryName': process.env.CLOUDINARY_NAME,
+        'cloudinaryApiKey': process.env.CLOUDINARY_API_KEY,
+        'cloudinaryPreset': process.env.CLOUDINARY_UPLOAD_PRESET
 
     })
 })
